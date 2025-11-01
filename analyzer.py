981c0ff5aa856643901e23b387c02bac240b4989
Jozef David Hernández Campos <jozefhdez2@gmail.com>
2025-11-01 17:03:21 -0600

    First commit

---
+++ analyzer.py
@@ -0,0 +1,10 @@
+import re
+import time
+
+
+def analyze_file(filename):
+    return 0
+
+
+if __name__ == '__main__':
+    analyze_file('sample.sql')
---
+++ sample.sql
@@ -0,0 +1,9 @@
+DECLARE
+  x NUMBER;
+BEGIN
+  y := 10;
+  x := 5;
+  RETURN;
+  x := x + 1;
+END;
+/
